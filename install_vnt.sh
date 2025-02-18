26d2bb69b1ea2fcd24175ed2fb8327ebe8d6260e
huang3370 <165783634+huang3370@users.noreply.github.com>
2025-02-18 21:09:29 +0800

    install_vnts.sh

--- install_vnt.sh
+++ install_vnt.sh
@@ -2,25 +2,25 @@
 #!/bin/bash
 
 # 下载并解压VNT客户端
-wget https://github.com/vnt-dev/vnt/releases/download/1.2.16/vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
-tar -zxvf vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
-cd vnt-x86_64-unknown-linux-musl-1.2.16
+wget https://github.com/vnt-dev/vnts/releases/download/v1.2.12/vnts-x86_64-unknown-linux-musl-v1.2.12.tar.gz
+tar -zxvf vnts-x86_64-unknown-linux-musl-v1.2.12.tar.gz
+cd vnts-x86_64-unknown-linux-musl-v1.2.12.tar.gz
 
 # 提示用户输入自定义参数
-echo "直接输入组网号："
-read id
-echo "输入-s空格IP:端口使用官方服务器直接回车下一步："
-read fwq
+echo "自定义虚拟IP默认直接回车下一步-例如-g空格192.168.x.x："
+read ip
+#echo "输入-s空格IP:端口使用官方服务器直接回车下一步："
+#read fwq
 
 # 创建一个服务文件
-cat <<EOF > /etc/systemd/system/vnt-cli.service
+cat <<EOF > /etc/systemd/system/vnts-cli.service
 [Unit]
 Description=VNT CLI Service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/root/vnt-cli -k $id $fwq --cmd
+ExecStart=/root/vnts $ip
 
 Restart=always
 User=root
@@ -34,7 +34,7 @@ EOF
 systemctl daemon-reload
 
 # 启用并启动服务
-systemctl enable vnt-cli.service
-systemctl start vnt-cli.service
+systemctl enable vnts-cli.service
+systemctl start vnts-cli.service
 
-echo "VNT客户端安装并设置开机自启动完成！"
+echo "VNTS客户端安装并设置开机自启动完成！"
